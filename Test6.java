1c9618beaa8946b090a7c406e3351b456295d010
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 12:22:03 +0530

    Create Test6.java

---
+++ Test6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test6 {
+public static void main(String[]args) {
+	String s="Hello";
+	int i,n;
+	Scanner scan=new Scanner(System.in);
+	n=scan.nextInt();
+	for(i=0;i<n;i++) {
+		System.out.println(s);
+	}
+}
+}
