7de806ca8e38c5e50d289de0f4b581a4abe38ff9
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 11:46:10 +0530

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Test3{
 public static void main(String[]args){
 Scanner scan=new Scanner(System.in);
-char c=scan.next();
+char c=scan.next().CharAt(0);
 if(Character.isLetter(c)){
 System.out.println("alphabet");
 }
