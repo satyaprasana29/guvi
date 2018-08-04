9a09dd6bdab1f3eee59b2adbfd0ff69fb795daa8
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 11:38:32 +0530

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test3{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+char c=scan.next();
+if(Character.isLetter(c)){
+System.out.println("alphabet");
+}
+}
+}
