4a078ec778f8fdb16be513d64730d57c49898447
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 11:15:53 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Test2{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+char c=scan.next();
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+System.out.println("vowel");
+}
+else{
+System.out.println("not vowel");
+}
+}
+}
