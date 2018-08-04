9ba8074722cd2a24f9b753bc75690a49ba588cdb
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 12:16:05 +0530

    Create Test5.java

---
+++ Test5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Test5{
+public static void main(String[]args){
+int a;
+Scanner scan=new Scanner(System.in);
+a=scan.nextInt();
+if(a%4==0){
+System.out.println("leap year");
+}
+}
+}
