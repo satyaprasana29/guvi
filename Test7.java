8aa9b8c056f5388ba44c83633a709e870f097da7
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 12:31:00 +0530

    Create Test7.java

---
+++ Test7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test7{
+public static void main(String[]args){
+int n,i,sum;
+Scanner scan=new Scanner(System.in);
+n=scan.nextInt();
+for(i=1;i<=n;i++){
+sum+=i;
+}
+System.out.println(sum);
+}
+}
