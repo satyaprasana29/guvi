98114f19db6e29eaf465f6e4c8746ab134d3ee1b
satyaprasana29 <42083667+satyaprasana29@users.noreply.github.com>
2018-08-04 11:02:14 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Test1{
+public static void main(String[]args){
+int a;
+Scanner scan=new Scanner(System.in);
+a=scan.nextInt();
+if(a%2==0){
+System.out.println("number is even");
+}
+else if(a%2==1){
+System.out.println("nember is odd");
+}
+}
+}
